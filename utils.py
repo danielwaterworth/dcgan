ea593dfa586932b39a6946a9dcb02c9b41fafd77
Daniel Waterworth <da.waterworth@gmail.com>
2017-08-28 13:18:04 -0500

    Removed parameters from get_image

--- utils.py
+++ utils.py
@@ -2,5 +2,5 @@ from random import shuffle
 import scipy.misc
 import numpy as np
 
-def get_image(image_path, image_size, is_crop=True, resize_w=64, is_grayscale = False):
+def get_image(image_path, resize_w):
     return scipy.misc.imresize(scipy.misc.imread(image_path), (resize_w, resize_w)) / 127.5 - 1
